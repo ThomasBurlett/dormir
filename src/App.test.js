8571a518258513ac8876ab99fea16f8d990ed278
Thomas Burlett <thomas.burlett@gmail.com>
2020-04-19 00:23:03 -0500

    Ripped out the hello world code

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Blank test', async () => {
+
+  expect(5).toBe(5);
 });
