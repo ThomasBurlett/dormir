beebcd1d0986f222e64ced2cda623833d6991688
Thomas Burlett <thomas.burlett@gmail.com>
2023-06-18 16:18:06 -0600

    build: testing CI/CD on custom domain

--- src/App.js
+++ src/App.js
@@ -2,5 +2,5 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 
 export default function App() {
-  return <Button variant="contained">Hello World, V2!</Button>;
+  return <Button variant="contained">Hello World, V3! We are now hosted on a custom domain!</Button>;
 }
