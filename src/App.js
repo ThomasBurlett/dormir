a63bf9ccf6a43a217cf9b68e90065068152e9bd4
Thomas Burlett <thomas.burlett@gmail.com>
2023-06-18 15:41:27 -0600

    build: testing CI/CD pipeline

--- src/App.js
+++ src/App.js
@@ -2,5 +2,5 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 
 export default function App() {
-  return <Button variant="contained">Hello World</Button>;
+  return <Button variant="contained">Hello World, V2!</Button>;
 }
