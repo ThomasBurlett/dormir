d815cc5a8564cc27aa7c4a53ed7d10e7f006bff5
Thomas Burlett <thomas.burlett@gmail.com>
2023-06-23 23:35:27 -0600

    feat: adding extra labeling

--- src/App.js
+++ src/App.js
@@ -84,22 +84,22 @@ export default function App() {
                 If you head to bed right now, you should try to wake up at one of the following times:
               </h2>
               <h3>
-                {format(add(timePickerValue, { hours: 1, minutes: 30 }), "hh:mm aa")}  - (1 Sleep Cycle)
+                {format(add(timePickerValue, { hours: 1, minutes: 30 }), "hh:mm aa")}  - (1 Sleep Cycle, 1 hour and 30 minutes)
               </h3>
               <h3>
-                {format(add(timePickerValue, { hours: 3 }), "hh:mm aa")}  - (2 Sleep Cycles)
+                {format(add(timePickerValue, { hours: 3 }), "hh:mm aa")}  - (2 Sleep Cycles, 3 hours)
               </h3>
               <h3>
-                {format(add(timePickerValue, { hours: 4, minutes: 30 }), "hh:mm aa")}  - (3 Sleep Cycles)
+                {format(add(timePickerValue, { hours: 4, minutes: 30 }), "hh:mm aa")}  - (3 Sleep Cycles, 4 hours and 30 minutes)
               </h3>
               <h3>
-                {format(add(timePickerValue, { hours: 6 }), "hh:mm aa")}  - (4 Sleep Cycles)
+                {format(add(timePickerValue, { hours: 6 }), "hh:mm aa")}  - (4 Sleep Cycles, 6 hours)
               </h3>
               <h3>
-                {format(add(timePickerValue, { hours: 7, minutes: 30 }), "hh:mm aa")}  - (5 Sleep Cycles)
+                {format(add(timePickerValue, { hours: 7, minutes: 30 }), "hh:mm aa")}  - (5 Sleep Cycles, 7 hours and 30 minutes)
               </h3>
               <h3>
-                {format(add(timePickerValue, { hours: 9 }), "hh:mm aa")}  - (6 Sleep Cycles)
+                {format(add(timePickerValue, { hours: 9 }), "hh:mm aa")}  - (6 Sleep Cycles, 9 hours)
               </h3>
               <p>
                 Please keep in mind that you should be falling asleep at these times.
