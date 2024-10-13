1d5df8553c6cf228d35639fc1500b06c332ca978
Thomas Burlett <thomas.burlett@gmail.com>
2024-10-12 21:14:03 -0600

    feat: project cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import { Button, defaultTheme, Provider } from "@adobe/react-spectrum"
-import moon from "/moon.svg"
-import "./App.css"
+import sleep from "./assets/sleep.svg"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,20 +8,12 @@ function App() {
   return (
     <Provider theme={defaultTheme}>
       <div>
-        <img src={moon} className="logo" alt="Vite logo" />
+        <img src={sleep} className="logo" alt="Duermo Logo" />
       </div>
       <h1>Duermo - A sleep calculator</h1>
-      <div className="card">
-        <Button
-          variant="primary"
-          onPress={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <Button variant="primary" onPress={() => setCount((count) => count + 1)}>
+        count is {count}
+      </Button>
     </Provider>
   )
 }
